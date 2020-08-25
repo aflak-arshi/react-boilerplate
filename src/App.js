3a8e6f6c44e5d916522df81f024042e13af87061
Aflak Arshi <aflakarshi@gmail.com>
2020-08-25 23:11:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <Welcome name="Aflak Arshi" />
+  );
+}
+
+export default App;
